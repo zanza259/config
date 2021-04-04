ced1f43148b3a9999a922317c12901ab1dfa1418
zanza259 <tesla259@gmail.com>
2021-04-04 09:51:05 -0400

    Create an additional downloads script and include nordvpn install details.

---
+++ additional_installs.sh
@@ -0,0 +1,4 @@
+# install nordvpn linux client
+# (https://support.nordvpn.com/Connectivity/Linux/1325531132/Installing-and-using-NordVPN-on-Debian-Ubuntu-Raspberry-Pi-Elementary-OS-and-Linux-Mint.htm)
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
